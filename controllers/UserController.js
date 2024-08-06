10a91a8ba340ddf4cf0e692d76266faed4d6e90d
thekaroe-thailand <thekaroe@gmail.com>
2024-08-06 20:09:55 +0700

    day10

--- controllers/UserController.js
+++ controllers/UserController.js
@@ -1,6 +1,6 @@
 const { PrismaClient } = require("@prisma/client");
 const prisma = new PrismaClient();
-
+const jwt = require("jsonwebtoken");
 const key = "my secret key for jwt of project fitness on Svelte";
 
 module.exports = {
@@ -12,6 +12,11 @@ module.exports = {
       if (u == "" || p == "") return res.status(401).send("unauthorized");
 
       const user = await prisma.user.findFirst({
+        select: {
+          id: true,
+          name: true,
+          level: true,
+        },
         where: {
           username: u,
           password: p,
@@ -20,6 +25,17 @@ module.exports = {
       });
 
       if (!user) return res.status(401).send("unauthorized");
+      const token = jwt.sign(user, key, { expiresIn: "30d" });
+
+      return res.send({ token: token });
+    } catch (e) {
+      return res.status(500).send({ error: e.message });
+    }
+  },
+  info: async (req, res) => {
+    try {
+      const payload = jwt.decode(req.headers.authorization, key);
+      return res.send(payload);
     } catch (e) {
       return res.status(500).send({ error: e.message });
     }
--- server.js
+++ server.js
@@ -3,9 +3,14 @@ const cors = require("cors");
 const bodyParser = require("body-parser");
 const app = express();
 
+const userController = require("./controllers/UserController");
+
 app.use(cors());
 app.use(bodyParser.urlencoded({ extended: true }));
 app.use(bodyParser.json());
+
+app.post("/api/user/signIn", (req, res) => userController.signIn(req, res));
+app.get("/api/user/info", (req, res) => userController.info(req, res));
 
 app.listen(3000, () => {
   console.log("API Server Started");
