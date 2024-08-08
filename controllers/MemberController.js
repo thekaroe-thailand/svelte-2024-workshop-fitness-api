5a88f6876dc89f434dc4bcbe506cf1e2b6b75d1d
thekaroe-thailand <thekaroe@gmail.com>
2024-08-08 20:25:03 +0700

    day11

--- controllers/MemberController.js
+++ controllers/MemberController.js
@@ -19,4 +19,44 @@ module.exports = {
       return res.status(500).send({ error: e.message });
     }
   },
+  list: async (req, res) => {
+    try {
+      const rows = await prisma.member.findMany();
+      return res.send({ results: rows });
+    } catch (e) {
+      return res.status(500).send({ error: e.message });
+    }
+  },
+  remove: async (req, res) => {
+    try {
+      await prisma.member.delete({
+        where: {
+          id: parseInt(req.params.id),
+        },
+      });
+      return res.send({ message: "success" });
+    } catch (e) {
+      return res.status(500).send({ error: e.message });
+    }
+  },
+  update: async (req, res) => {
+    try {
+      await prisma.member.update({
+        data: {
+          name: req.body.name,
+          phone: req.body.phone,
+          gender: req.body.gender,
+          registerDate: new Date(req.body.registerDate),
+          expireDate: new Date(req.body.expireDate),
+        },
+        where: {
+          id: parseInt(req.params.id),
+        },
+      });
+
+      return res.send({ message: "success" });
+    } catch (e) {
+      return res.status(500).send({ error: e.message });
+    }
+  },
 };
--- server.js
+++ server.js
@@ -13,6 +13,13 @@ app.use(bodyParser.json());
 app.post("/api/user/signIn", (req, res) => userController.signIn(req, res));
 app.get("/api/user/info", (req, res) => userController.info(req, res));
 app.post("/api/member/create", (req, res) => memberController.create(req, res));
+app.get("/api/member/list", (req, res) => memberController.list(req, res));
+app.delete("/api/member/remove/:id", (req, res) =>
+  memberController.remove(req, res)
+);
+app.put("/api/member/update/:id", (req, res) =>
+  memberController.update(req, res)
+);
 
 app.listen(3000, () => {
   console.log("API Server Started");
